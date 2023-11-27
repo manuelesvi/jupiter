172d1a1c33b7a52b44f08a315ed18b0d2e2a47f6
Manuel Espinosa <106558272+manuelesvi@users.noreply.github.com>
2023-11-27 17:57:26 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,8 @@
 #include <cgraph.h>
 #include <cdt.h>
 
-int main(int argc, char * argv[])
+int 
+main(int argc, char * argv[])
 {
         printf("Hello from %s!\n", argv[0]);
 	
@@ -14,20 +15,20 @@ int main(int argc, char * argv[])
 	// parse command line args - minimally argv[0] sets layout engine
 	// gvParseArgs(gvc, argc, argv);
 
-	// Create a simple digraph
+	// Create a directed graph with 2 nodes (n, m)	
 	Agraph_t *g = agopen("g", Agdirected, 0);
 	Agnode_t *n = agnode(g, "n", 1);
 	Agnode_t *m = agnode(g, "m", 1);
+	// connected with an edge.
 	agedge(g, n, m, 0, 1);
 
 	// Compute a layout using layout engine from command line args
 	// gvLayoutJobs(gvc, g);
 	
 	gvLayout(gvc, g, "dot");
-
-	// Write the graph according to -T and -o options
-	// gvRenderJobs(gvc, g);
+		
 	FILE *file = fopen("test.png", "w");
+	// Write the graph according to -T and -o options
 	gvRender(gvc, g, "png", file);
 	fflush(file);
 	fclose(file);
